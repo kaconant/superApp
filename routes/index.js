ab7603079d4c2e61428d3de2d4137616e5bb9db0
Krissy Conant <kconant@KConant-MacBook-Pro.local>
2018-05-19 12:10:31 -0400

    followed practice example

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let dataForTemplate = {
+    title: 'Express',
+    myName: 'krissy conant',
+    cats: ['oakley', 'milla']
+  };
+  
+  res.render('index', dataForTemplate);
 });
 
 module.exports = router;
